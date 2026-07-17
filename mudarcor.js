1bde867d2358ebd410c6907aa3c74598567b5eab
EduardoRocha81 <rochadudu@hotmail.com>
2026-07-17 15:13:15 -0300

    Create mudarcor.js

---
+++ mudarcor.js
@@ -0,0 +1,21 @@
+function trocarCor() 
+{
+    const div1 = document.getElementById("div-1");
+    const div2 = document.getElementById("div-2");
+    const div3 = document.getElementById("div-3");
+
+div1.style.backgroundColor = "red";
+div2.style.backgroundColor = "lightblue";
+div3.style.backgroundColor = "lightgreen";
+}
+
+function retornarCor()
+{
+    const div1 = document.getElementById("div-1");
+    const div2 = document.getElementById("div-2");
+    const div3 = document.getElementById("div-3");
+
+div1.style.backgroundColor = "";
+div2.style.backgroundColor = "";
+div3.style.backgroundColor = "";
+}
